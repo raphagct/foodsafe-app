966977007a1561af0a61a58517fd66553d2cbc93
Raphaël Guichet <raphael.guichet@etu.unice.fr>
2026-06-29 10:49:40 +0200

    feat: initialize iOS platform support and configure native project assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,8 +4,8 @@ const config = {
   webDir: 'dist',
   server: {
     androidScheme: 'https',
-    url: 'http://10.0.2.2:5173', // Dit à Capacitor d'aller chercher ton serveur VS Code
-    cleartext: true              // Autorise Android à charger du HTTP sans bloquer (évite l'écran blanc)
+    // url: 'http://10.0.2.2:5173', // Décommenté — cette adresse ne fonctionne que sur Android emulator
+    cleartext: true
   },
   windowsAndroidStudioPath: "C:\\Users\\utilisateur\\AppData\\Local\\Programs\\Android Studio\\bin\\studio64.exe"
 };
