d67b8531da0f08a5db6ed0b5050331a3229673bc
Pistone Noa <noa.pistone@gmail.com>
2026-06-30 17:11:56 +0700

    Android Version

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,15 @@ export default defineConfig({
       targets: ['chrome >= 74'], // Force la compatibilité avec les navigateurs mobiles plus anciens
     }),
   ],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: {
+        chrome: 74 << 16
+      }
+    }
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  }
 })
